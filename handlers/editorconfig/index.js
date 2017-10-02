66ed03bc1cc2516cf2f644348a6b57741448137a
Jouderian Nobre <jouderianjr@gmail.com>
2017-10-02 10:50:07 -0300

    Remove specific OS commands (cp, cd...)

--- handlers/editorconfig/index.js
+++ handlers/editorconfig/index.js
@@ -1,8 +1,6 @@
-const child_process = require('child-process-promise')
+const fs = require('fs-extra')
 const emoji = require('node-emoji')
 const chalk = require('chalk')
-
-const exec = child_process.exec
 
 const question = {
   name: 'editorconfig',
@@ -15,7 +13,7 @@ const func = (cwd, folderName) => {
   console.log(`${emoji.get('fire')}  ${chalk.cyan('Adding Editor Config file')} ${emoji.get('fire')}`)
   console.log('\n\n')
 
-  return exec(`cp ${__dirname}/.editorconfig ${cwd}/${folderName}`)
+  return fs.copy(`${__dirname}/.editorconfig`, `${cwd}/${folderName}/.editorconfig`)
 }
 
 const action = {
